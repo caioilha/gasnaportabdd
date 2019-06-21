b434776887bf4e1fd5f8cf03028819a7deb56a0f
Caio <carlos.ilha@acad.pucrs.br>
2019-06-20 23:12:54 -0300

    Gherkin added

--- android_sample/app/src/main/kotlin/org/spekframework/speksample/SupplierValidator.kt
+++ android_sample/app/src/main/kotlin/org/spekframework/speksample/SupplierValidator.kt
@@ -2,7 +2,9 @@ package org.spekframework.speksample
 
 class SupplierValidator(val supplier: Supplier){
 
-    public fun validateCoordinates(): Boolean {
+    fun validateSupplier(): Boolean = validateData() && validateCoordinates()
+
+    fun validateCoordinates(): Boolean {
         if (supplier.coordinates[0] == 0f && supplier.coordinates[1] == 0f) {
             //Snackbar.make(layoutAddSupplier, "Endereço não encontrado.", Snackbar.LENGTH_LONG).show()
             return false
@@ -10,7 +12,7 @@ class SupplierValidator(val supplier: Supplier){
         return true //retorna false se os 2 forem 0 (n achou endereço ou algo assim), que é no meio do oceano e parece ser um local complicado de algum dia existir fornecedor lá
     }
 
-    public fun validateData(): Boolean {
+    fun validateData(): Boolean {
         if (nameIsBlank() || addressIsBlank() || phoneIsBlank()) {
             //Snackbar.make(layoutAddSupplier, "Todos os dados são obrigatórios.", Snackbar.LENGTH_LONG).show()
             return false
@@ -21,14 +23,13 @@ class SupplierValidator(val supplier: Supplier){
         return true
     }
 
-    public fun nameIsBlank(): Boolean = supplier.name.isBlank()
+    fun nameIsBlank(): Boolean = supplier.name.isBlank()
 
-    public fun addressIsBlank(): Boolean = supplier.address.isBlank()
+    fun addressIsBlank(): Boolean = supplier.address.isBlank()
 
-    public fun phoneIsBlank(): Boolean = supplier.phone.isBlank()
+    fun phoneIsBlank(): Boolean = supplier.phone.isBlank()
 
-
-    public fun validatePrices(): Boolean{
+    fun validatePrices(): Boolean{
         if (supplier.priceP13 == 0.0 && supplier.priceP2 == 0.0 && supplier.priceP45 == 0.0) {
             //Snackbar.make(layoutAddSupplier, "O Distribuidor deve possuir ao menos um tipo de gás.", Snackbar.LENGTH_LONG).show()
             return false
--- android_sample/app/src/test/kotlin/org/spekframework/speksample/SupplierSpec.kt
+++ android_sample/app/src/test/kotlin/org/spekframework/speksample/SupplierSpec.kt
@@ -1,25 +1,27 @@
 package org.spekframework.speksample
 
 import org.spekframework.spek2.Spek
+import org.spekframework.spek2.style.gherkin.Feature
 import org.spekframework.spek2.style.specification.describe
 import kotlin.test.assertEquals
 
 object SupplierSpec : Spek({
     describe("A valid supplier") {
-        val supplier by memoized { SupplierValidator( supplier = Supplier(
-                coordinates = floatArrayOf(1f , 2f),
-                priceP2 = 1.0,//: Double,
-                priceP13 = 2.0,
-                priceP45 = 3.0,
-                _id = "whatever",
-                address = "Asdrdrses",
-                name = "Namezoid",
-                scoresAvg = 0.0,
-                phone = "0129375656278237590267934559562",
-                distance = 0.0,
-                isFavorite = false
-        ))}
-
+        val supplier by memoized {
+            SupplierValidator(supplier = Supplier(
+                    coordinates = floatArrayOf(1f, 2f),
+                    priceP2 = 1.0,//: Double,
+                    priceP13 = 2.0,
+                    priceP45 = 3.0,
+                    _id = "whatever",
+                    address = "Asdrdrses",
+                    name = "Namezoid",
+                    scoresAvg = 0.0,
+                    phone = "(66)6666.6666",
+                    distance = 0.0,
+                    isFavorite = false
+            ))
+        }
         it("should not have \"invalid\" coordinates") {
             supplier.supplier.coordinates = floatArrayOf(0f, 0f)
             assertEquals(expected = false, actual = supplier.validateCoordinates())
@@ -28,7 +30,7 @@ object SupplierSpec : Spek({
             assertEquals(expected = true, actual = supplier.validateCoordinates())
         }
 
-        it("should have an address"){
+        it("should have an address") {
             assertEquals(expected = false, actual = supplier.addressIsBlank())
 
             supplier.supplier.address = ""
@@ -36,22 +38,127 @@ object SupplierSpec : Spek({
 
         }
 
-        it("should have a phone"){
+        it("should have a phone") {
             assertEquals(expected = false, actual = supplier.phoneIsBlank())
 
             supplier.supplier.phone = ""
             assertEquals(expected = true, actual = supplier.phoneIsBlank())
         }
 
-        it("should have a name"){
+        it("should have a name") {
             assertEquals(expected = false, actual = supplier.nameIsBlank())
 
             supplier.supplier.name = ""
             assertEquals(expected = true, actual = supplier.nameIsBlank())
         }
 
-        it("should be                        valid)") {
-            assertEquals(expected = true, actual = supplier.validateData() && supplier.validateCoordinates())
+        it("should have at least one gas price") {
+
+            supplier.supplier.priceP2 = 0.0
+            supplier.supplier.priceP13 = 0.0
+            supplier.supplier.priceP45 = 0.0
+
+            assertEquals(expected = false, actual = supplier.validatePrices())
+
+            supplier.supplier.priceP13 = 1.0
+            assertEquals(expected = true, actual = supplier.validatePrices())
+        }
+
+    }
+    Feature("Validations when adding a supplier") {
+        val supplier by memoized {
+            SupplierValidator(supplier = Supplier(
+                    coordinates = floatArrayOf(1f, 2f),
+                    priceP2 = 1.0,//: Double,
+                    priceP13 = 2.0,
+                    priceP45 = 3.0,
+                    _id = "whatever",
+                    address = "Asdrdrses",
+                    name = "Namezoid",
+                    scoresAvg = 0.0,
+                    phone = "(66)6666.6666",
+                    distance = 0.0,
+                    isFavorite = false
+            ))
+        }
+
+        Scenario("invalid coordinates should not validate") {
+            var result = false
+            Given("a supplier with invalid coordinates") {
+                supplier.supplier.coordinates = floatArrayOf(0f, 0f)
+            }
+
+            When("the supplier is validated") {
+                result = supplier.validateSupplier()
+            }
+
+            Then("it should not be allowed to be added") {
+                assertEquals(false, result)
+            }
+
+        }
+
+        Scenario("empty names should not validate") {
+            var result = false
+            Given("a supplier with an empty name") {
+                supplier.supplier.name = ""
+            }
+
+            When("the supplier is validated") {
+                result = supplier.validateSupplier()
+            }
+
+            Then("it should not be allowed to be added") {
+                assertEquals(false, result)
+            }
+
+        }
+
+        Scenario("empty addresses should not validate") {
+            var result = false
+            Given("a supplier with an empty address") {
+                supplier.supplier.address = ""
+            }
+
+            When("the supplier is validated") {
+                result = supplier.validateSupplier()
+            }
+
+            Then("it should not be allowed to be added") {
+                assertEquals(false, result)
+            }
+        }
+
+        Scenario("empty phone numbers should not validate") {
+            var result = false
+            Given("a supplier with an empty phone number") {
+                supplier.supplier.phone = ""
+            }
+
+            When("the supplier is validated") {
+                result = supplier.validateSupplier()
+            }
+
+            Then("it should not be allowed to be added") {
+                assertEquals(false, result)
+            }
+        }
+
+        Scenario("empty gas prices should not validate") {
+            var result = false
+            Given("a supplier with no gas prices") {
+                supplier.supplier.priceP2 = 0.0
+                supplier.supplier.priceP13 = 0.0
+                supplier.supplier.priceP45 = 0.0
+            }
+
+            When("the supplier is validated") {
+                result = supplier.validateSupplier()
+            }
+
+            Then("it should not be allowed to be added") {
+                assertEquals(false, result)
+            }
         }
     }
 })
